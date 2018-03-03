ed33f8a7a1b4c1cfeaad08f18721c9548c224f32
Rui Lopes <rgl@ruilopes.com>
2018-03-03 09:25:15 +0000

    enable tls 1.1+ in the provision script

--- provision.ps1
+++ provision.ps1
@@ -1,7 +1,6 @@
 Set-StrictMode -Version Latest
-
+$ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = 'Stop'
-
 trap {
     #Write-Host
     #Write-Host 'whoami from autounattend:'
@@ -12,11 +11,17 @@ trap {
     Write-Host
     Write-Host "ERROR: $_"
     Write-Host (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Write-Host (($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1')
     Write-Host
     Write-Host 'Sleeping for 60m to give you time to look around the virtual machine before self-destruction...'
     Start-Sleep -Seconds (60*60)
     Exit 1
 }
+
+# enable TLS 1.1 and 1.2.
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol `
+    -bor [Net.SecurityProtocolType]::Tls11 `
+    -bor [Net.SecurityProtocolType]::Tls12
 
 if (![Environment]::Is64BitProcess) {
     throw 'this must run in a 64-bit PowerShell session'
