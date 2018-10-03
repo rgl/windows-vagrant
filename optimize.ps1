3701c17ad8269a43451dbf7eca4810aa92ca69eb
Rui Lopes <rgl@ruilopes.com>
2018-10-03 22:47:44 +0100

    clean the WinSxS folder

--- optimize.ps1
+++ optimize.ps1
@@ -90,6 +90,29 @@ Write-Host 'Running Automatic Maintenance...'
 MSchedExe.exe Start
 Wait-Condition {@(Get-ScheduledTasks | Where-Object {($_.State -ge 4) -and (Test-IsMaintenanceTask $_.XML)}).Count -eq 0} -DebounceSeconds 60
 
+# NB even thou the automatic maintenance includes a component cleanup task,
+#    it will not clean everything, as such, dism will clean the rest.
+# NB to analyse the used space use: dism.exe /Online /Cleanup-Image /AnalyzeComponentStore
+# see https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/clean-up-the-winsxs-folder
+Write-Host 'Cleaning up the WinSxS folder...'
+dism.exe /Online /Quiet /Cleanup-Image /StartComponentCleanup /ResetBase
+if ($LASTEXITCODE) {
+    throw "Failed with Exit Code $LASTEXITCODE"
+}
+
+# NB even after cleaning up the WinSxS folder the "Backups and Disabled Features"
+#    field of the analysis report will display a non-zero number because the
+#    disabled features packages are still on disk. you can remove them with:
+#       Get-WindowsOptionalFeature -Online `
+#           | Where-Object {$_.State -eq 'Disabled'} `
+#           | ForEach-Object {
+#               Write-Host "Removing feature $($_.FeatureName)..."
+#               dism.exe /Online /Quiet /Disable-Feature "/FeatureName:$($_.FeatureName)" /Remove
+#           }
+#    NB a removed feature can still be installed from other sources (e.g. windows update).
+Write-Host 'Analyzing the WinSxS folder...'
+dism.exe /Online /Cleanup-Image /AnalyzeComponentStore
+
 
 #
 # reclaim the free disk space.
