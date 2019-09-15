5087e87eb9192cecd8ce19e4807a992880125650
Rui Lopes <rgl@ruilopes.com>
2019-09-15 20:11:02 +0100

    show the windows product-key

--- example/summary.ps1
+++ example/summary.ps1
@@ -147,6 +147,80 @@ Write-Output "$(Get-MachineSID)"
 Write-Title 'Windows License'
 cscript -nologo c:/windows/system32/slmgr.vbs -dlv
 
+Write-Title 'Windows Product-Key'
+# NB this C# code came from
+#       https://github.com/mrpeardotnet/WinProdKeyFinder/blob/d05bd525214d571a24298830cccaa466b22f3c87/WinProdKeyFind/KeyDecoder.cs
+#    and was slightly modified to work only in Windows8+/PowerShell.
+Add-Type @'
+using System;
+using Microsoft.Win32;
+
+namespace WinProdKeyFind
+{
+    public static class KeyDecoder
+    {
+        public static string GetWindowsProductKeyFromRegistry()
+        {
+            var localKey =
+                RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, Environment.Is64BitOperatingSystem
+                    ? RegistryView.Registry64
+                    : RegistryView.Registry32);
+            var registryKeyValue = localKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue("DigitalProductId");
+            if (registryKeyValue == null)
+                return "Failed to get DigitalProductId from registry";
+            var digitalProductId = (byte[])registryKeyValue;
+            localKey.Close();
+            return DecodeProductKeyWin8AndUp(digitalProductId);
+        }
+
+        /// <summary>
+        /// Decodes Windows Product Key from the DigitalProductId. 
+        /// This method applies to DigitalProductId from Windows 8 or newer versions of Windows.
+        /// </summary>
+        /// <param name="digitalProductId">DigitalProductId to decode</param>
+        /// <returns>Decoded Windows Product Key as a string</returns>
+        public static string DecodeProductKeyWin8AndUp(byte[] digitalProductId)
+        {
+            var key = String.Empty;
+            const int keyOffset = 52;
+            var isWin8 = (byte)((digitalProductId[66] / 6) & 1);
+            digitalProductId[66] = (byte)((digitalProductId[66] & 0xf7) | (isWin8 & 2) * 4);
+
+            const string digits = "BCDFGHJKMPQRTVWXY2346789";
+            var last = 0;
+            for (var i = 24; i >= 0; i--)
+            {
+                var current = 0;
+                for (var j = 14; j >= 0; j--)
+                {
+                    current = current*256;
+                    current = digitalProductId[j + keyOffset] + current;
+                    digitalProductId[j + keyOffset] = (byte)(current/24);
+                    current = current%24;
+                    last = current;
+                }
+                key = digits[current] + key;
+            }
+
+            var keypart1 = key.Substring(1, last);
+            var keypart2 = key.Substring(last + 1, key.Length - (last + 1));
+            key = keypart1 + "N" + keypart2;
+
+            for (var i = 5; i < key.Length; i += 6)
+            {
+                key = key.Insert(i, "-");
+            }
+
+            return key;
+        }
+    }
+}
+'@
+function Get-WindowsProductKey {
+    [WinProdKeyFind.KeyDecoder]::GetWindowsProductKeyFromRegistry()
+}
+Get-WindowsProductKey
+
 Write-Title 'Installed Windows Updates'
 function Get-InstalledWindowsUpdates {
     $session = New-Object -ComObject 'Microsoft.Update.Session'
