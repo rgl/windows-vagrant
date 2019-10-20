2d885e3198328339040b9798c3449ec8242a8041
Rui Lopes <rgl@ruilopes.com>
2019-10-20 22:21:01 +0100

    install cloudbase-init

---
+++ provision-cloudbase-init.ps1
@@ -0,0 +1,83 @@
+Set-StrictMode -Version Latest
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+trap {
+    Write-Host
+    Write-Host "ERROR: $_"
+    Write-Host (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Write-Host (($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1')
+    Write-Host
+    Write-Host 'Sleeping for 60m to give you time to look around the virtual machine before self-destruction...'
+    Start-Sleep -Seconds (60*60)
+    Exit 1
+}
+
+$cloudbaseInitHome = 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init'
+$cloudbaseInitConfPath = "$cloudbaseInitHome\conf\cloudbase-init.conf"
+$cloudbaseInitPackagePath = "$cloudbaseInitHome\Python\Lib\site-packages\cloudbaseinit"
+
+$artifactUrl = 'https://www.cloudbase.it/downloads/CloudbaseInitSetup_x64.msi'
+$artifactPath = "$env:TEMP\$(Split-Path -Leaf $artifactUrl)"
+$artifactLogPath = "$artifactPath.log"
+
+Write-Host 'Downloading the cloudbase-init setup...'
+(New-Object System.Net.WebClient).DownloadFile($artifactUrl, $artifactPath)
+
+Write-Host 'Installing cloudbase-init...'
+# NB this also installs the cloudbase-init service, which will automatically start on the next boot.
+# see https://github.com/cloudbase/cloudbase-init-installer
+msiexec /i $artifactPath /qn /l*v $artifactLogPath | Out-String -Stream
+if ($LASTEXITCODE) {
+    throw "Failed with Exit Code $LASTEXITCODE"
+}
+
+Write-Host 'Downloading the rgl/cloudbase-init no-cloud branch...'
+$artifactUrl = 'https://github.com/rgl/cloudbase-init/archive/add-no-cloud.zip'
+$artifactPath = "$env:TEMP\$(Split-Path -Leaf $artifactUrl)"
+(New-Object System.Net.WebClient).DownloadFile($artifactUrl, $artifactPath)
+
+Write-Host 'Replacing the cloudbaseinit package with rgl/cloudbase-init...'
+Expand-Archive $artifactPath "$artifactPath-extracted"
+$cloudbaseInitTmpPackagePath = Resolve-Path "$artifactPath-extracted\*\cloudbaseinit"
+Remove-Item -Recurse $cloudbaseInitPackagePath
+Copy-Item -Recurse $cloudbaseInitTmpPackagePath $cloudbaseInitPackagePath
+Remove-Item -Recurse "$artifactPath-extracted"
+
+Write-Host 'Replacing the configuration...'
+# The default configuration is:
+#   [DEFAULT]
+#   username=Admin
+#   groups=Administrators
+#   inject_user_password=true
+#   config_drive_raw_hhd=true
+#   config_drive_cdrom=true
+#   config_drive_vfat=true
+#   bsdtar_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\bsdtar.exe
+#   mtools_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\
+#   verbose=true
+#   debug=true
+#   logdir=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\log\
+#   logfile=cloudbase-init.log
+#   default_log_levels=comtypes=INFO,suds=INFO,iso8601=WARN,requests=WARN
+#   logging_serial_port_settings=
+#   mtu_use_dhcp_config=true
+#   ntp_use_dhcp_config=true
+#   local_scripts_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts\
+# see https://cloudbase-init.readthedocs.io/en/latest/tutorial.html#configuration-file
+# see https://cloudbase-init.readthedocs.io/en/latest/config.html#config-list
+Set-Content -Encoding ascii $cloudbaseInitConfPath @"
+[DEFAULT]
+username=vagrant
+groups=Administrators
+first_logon_behaviour=no
+debug=true
+log_dir=$cloudbaseInitHome\log
+log_file=cloudbase-init.log
+bsdtar_path=$cloudbaseInitHome\bin\bsdtar.exe
+mtools_path=$cloudbaseInitHome\bin\
+metadata_services=cloudbaseinit.metadata.services.nocloud.NoCloudConfigDriveService
+
+[config_drive]
+locations=cdrom
+types=iso
+"@
