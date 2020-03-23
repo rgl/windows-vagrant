d589dcd38acc5e0558e5145dd04750a6ee73b4d5
Rui Lopes <rgl@ruilopes.com>
2020-03-23 20:10:59 +0000

    properly execute the provisioning powershell scripts

---
+++ example/provision-chocolatey.ps1
@@ -0,0 +1,1 @@
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
---
+++ example/ps.ps1
@@ -0,0 +1,48 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$script
+)
+
+Set-StrictMode -Version Latest
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+trap {
+    Write-Output "ERROR: $_"
+    Write-Output (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Write-Output (($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1')
+    Exit 1
+}
+
+# enable TLS 1.2.
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol `
+    -bor [Net.SecurityProtocolType]::Tls12
+
+# wrap the choco command (to make sure this script aborts when it fails).
+function Start-Choco([string[]]$Arguments, [int[]]$SuccessExitCodes=@(0)) {
+    $command, $commandArguments = $Arguments
+    if ($command -eq 'install') {
+        $Arguments = @($command, '--no-progress') + $commandArguments
+    }
+    for ($n = 0; $n -lt 10; ++$n) {
+        if ($n) {
+            # NB sometimes choco fails with "The package was not found with the source(s) listed."
+            #    but normally its just really a transient "network" error.
+            Write-Host "Retrying choco install..."
+            Start-Sleep -Seconds 3
+        }
+        &C:\ProgramData\chocolatey\bin\choco.exe @Arguments
+        if ($SuccessExitCodes -Contains $LASTEXITCODE) {
+            return
+        }
+    }
+    throw "$(@('choco')+$Arguments | ConvertTo-Json -Compress) failed with exit code $LASTEXITCODE"
+}
+function choco {
+    Start-Choco $Args
+}
+
+cd c:/vagrant
+$script = Resolve-Path $script
+cd (Split-Path $script -Parent)
+Write-Host "Running $script..."
+. $script
