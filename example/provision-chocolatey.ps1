267dc895c08953d8a92685f137c5c431dcdcca3f
Rui Lopes <rgl@ruilopes.com>
2022-03-25 08:43:00 +0000

    update the chocolatey install script location to the community address

--- example/provision-chocolatey.ps1
+++ example/provision-chocolatey.ps1
@@ -1,1 +1,1 @@
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
