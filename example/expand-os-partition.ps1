f6c6b99f8f1e8794ff8f38e0ca0f47807d445b0b
Rui Lopes <rgl@ruilopes.com>
2022-10-31 14:39:27 +0000

    align the partition size

--- example/expand-os-partition.ps1
+++ example/expand-os-partition.ps1
@@ -1,7 +1,12 @@
 # expand the C drive when there is disk available.
 $partition = Get-Partition -DriveLetter C
 $partitionSupportedSize = Get-PartitionSupportedSize -DriveLetter C
-if ($partition.Size -ne $partitionSupportedSize.SizeMax) {
-    Write-Host "Expanding the C: partition from $($partition.Size) to $($partitionSupportedSize.SizeMax) bytes..."
-    Resize-Partition -DriveLetter C -Size $partitionSupportedSize.SizeMax
+# calculate the maximum size (1MB aligned).
+# NB when running in the hyperv hypervisor, the size must be must multiple of
+#    1MB, otherwise, it fails with:
+#       The size of the extent is less than the minimum of 1MB.
+$sizeMax = $partitionSupportedSize.SizeMax - ($partitionSupportedSize.SizeMax % (1*1024*1024))
+if ($partition.Size -lt $sizeMax) {
+    Write-Host "Expanding the C: partition from $($partition.Size) to $sizeMax bytes..."
+    Resize-Partition -DriveLetter C -Size $sizeMax
 }
