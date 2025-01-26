42fcffa88dabdb0880a550f1a47549570cee2837
Rui Lopes <rgl@ruilopes.com>
2025-01-26 15:31:49 +0000

    upgrade to powershell 7.4.7

--- provision-pwsh.ps1
+++ provision-pwsh.ps1
@@ -26,10 +26,10 @@ $env:POWERSHELL_UPDATECHECK = 'Off'
 
 # install powershell lts.
 # see https://github.com/PowerShell/PowerShell/releases
-# renovate: datasource=github-releases depName=PowerShell/PowerShell
-$archiveVersion = '7.4.6'
+# renovate: datasource=github-releases depName=PowerShell/PowerShell extractVersion=^v(?<version>7\.4\..+)
+$archiveVersion = '7.4.7'
 $archiveUrl = "https://github.com/PowerShell/PowerShell/releases/download/v$archiveVersion/PowerShell-$archiveVersion-win-x64.msi"
-$archiveHash = 'ed331a04679b83d4c013705282d1f3f8d8300485eb04c081f36e11eaf1148bd0'
+$archiveHash = '217b5575398d5e4a0ac67aff7b468c696a66c3e209685cf338032fc0a5f032ec'
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 
