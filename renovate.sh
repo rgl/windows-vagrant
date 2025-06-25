5c21c95c906a0ac2cd88ae5b6b4347a2ebca9c2c
Rui Lopes <rgl@ruilopes.com>
2025-06-25 06:12:52 +0000

    upgrade to gitea 1.24.2

--- renovate.sh
+++ renovate.sh
@@ -13,7 +13,7 @@ gitea_container_name="$(basename "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")
 
 # see https://hub.docker.com/r/gitea/gitea/tags
 # renovate: datasource=docker depName=gitea/gitea
-gitea_version='1.23.8'
+gitea_version='1.24.2'
 
 # see https://hub.docker.com/r/renovate/renovate/tags
 # renovate: datasource=docker depName=renovate/renovate
@@ -29,7 +29,7 @@ install -d tmp
 # start gitea in background.
 # see https://docs.gitea.io/en-us/config-cheat-sheet/
 # see https://github.com/go-gitea/gitea/releases
-# see https://github.com/go-gitea/gitea/blob/v1.23.8/docker/root/etc/s6/gitea/setup
+# see https://github.com/go-gitea/gitea/blob/v1.24.2/docker/root/etc/s6/gitea/setup
 echo 'Starting Gitea...'
 docker run \
     --detach \
