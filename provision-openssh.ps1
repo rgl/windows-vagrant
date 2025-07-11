a769eeecb96644084ab3c5eacd17f50aaad7d509
Rui Lopes <rgl@ruilopes.com>
2025-07-11 06:01:12 +0000

    upgrade to rsync 3.4.1-20250710

--- provision-openssh.ps1
+++ provision-openssh.ps1
@@ -46,12 +46,12 @@ function Install-ZippedApplication($destinationPath, $name, $url, $expectedHash,
 function Install-Rsync {
     # see https://github.com/rgl/rsync-vagrant/releases
     # renovate: datasource=github-releases depName=rgl/rsync-vagrant
-    $version = '3.4.1-20250411'
+    $version = '3.4.1-20250710'
     Install-ZippedApplication `
         $rsyncHome `
         rsync `
         "https://github.com/rgl/rsync-vagrant/releases/download/v$version/rsync-vagrant-$version.zip" `
-        8a4492df681a66074b59513f4446ef660fe451187b2c943096fcde46a5c17a21
+        a2718a9d3751612c47d05dea1f0a05b36e9719049c11521c90a3640fa38da330
     [Environment]::SetEnvironmentVariable(
         'PATH',
         "$([Environment]::GetEnvironmentVariable('PATH', 'Machine'));$rsyncHome",
